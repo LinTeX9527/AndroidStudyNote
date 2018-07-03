e9b17c9b6c957301d4056f107d376e95ee835ec8
lintex9527 <lintex9527@yeah.net>
2018-07-03 14:55:49 +0800

    FrameLayout基础

---
+++ runoob-framelayout/src/main/java/com/lintex9527/runoob_framelayout/MainActivity.java
@@ -0,0 +1,20 @@
+package com.lintex9527.runoob_framelayout;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+
+/**
+ * 随手指移动的妹子图
+ * 教程链接：
+ * http://www.runoob.com/w3cnote/android-tutorial-framelayout.html
+ *
+ */
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
