5b63830d396306a9debb9d010fe12950ab3194df
lintex9527 <lintex9527@yeah.net>
2018-07-27 10:54:01 +0800

    ContextMenu 上下文菜单

--- runoob/src/main/java/com/lintex9527/runoob/TestMenusActivity.java
+++ runoob/src/main/java/com/lintex9527/runoob/TestMenusActivity.java
@@ -3,8 +3,11 @@ package com.lintex9527.runoob;
 import android.graphics.Color;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.ContextMenu;
 import android.view.Menu;
+import android.view.MenuInflater;
 import android.view.MenuItem;
+import android.view.View;
 import android.widget.TextView;
 
 
@@ -17,6 +20,7 @@ import android.widget.TextView;
  * 2、SubMenu: 子菜单，点击子菜单项将弹出一个子菜单项的悬浮框，子菜单不支持嵌套
  * 3、ContextMenu: 上下文菜单，通过长按某个视图组件后出现的菜单，该组件需注册上下文菜单
  *
+ * 【注意】子菜单和上下文菜单都无法显示图标，只有OptionsMenu才能显示图标。
  *
  * 关于OptionsMenu，一般只用重写下面两个方法即可
  * public boolean onCreateOptionsMenu(Menu menu) 初始化菜单项
@@ -34,6 +38,7 @@ public class TestMenusActivity extends AppCompatActivity {
     private final int BLACK = 116;
 
     private TextView tv_test;
+    private TextView tv_context;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -41,6 +46,10 @@ public class TestMenusActivity extends AppCompatActivity {
         setContentView(R.layout.activity_test_menus);
 
         tv_test = (TextView) findViewById(R.id.tv_test);
+
+        tv_context = (TextView) findViewById(R.id.tv_context);
+        // 必须调用方法 registerForContextMenu(View) 来显示地为某一个View注册上下文菜单
+        registerForContextMenu(tv_context);
     }
 
     /**
@@ -102,5 +111,44 @@ public class TestMenusActivity extends AppCompatActivity {
         }
 
         return super.onOptionsItemSelected(item);
+    }
+
+
+    /**
+     * 创建上下文菜单
+     * @param menu
+     * @param v
+     * @param menuInfo
+     */
+    @Override
+    public void onCreateContextMenu(ContextMenu menu, View v, ContextMenu.ContextMenuInfo menuInfo) {
+        // 只用添加下面这两行代码就可以添加上下文菜单
+        MenuInflater inflater = new MenuInflater(this);
+        inflater.inflate(R.menu.menu_context, menu);
+
+        super.onCreateContextMenu(menu, v, menuInfo);
+    }
+
+
+    /**
+     * 上下文菜单被点击时的触发事件
+     * @param item 被点击的菜单项
+     * @return
+     */
+    @Override
+    public boolean onContextItemSelected(MenuItem item) {
+
+        switch (item.getItemId()) {
+            case R.id.blue:
+                tv_context.setTextColor(Color.BLUE);
+                break;
+            case R.id.green:
+                tv_context.setTextColor(Color.GREEN);
+                break;
+            case R.id.red:
+                tv_context.setTextColor(Color.RED);
+                break;
+        }
+        return super.onContextItemSelected(item);
     }
 }
