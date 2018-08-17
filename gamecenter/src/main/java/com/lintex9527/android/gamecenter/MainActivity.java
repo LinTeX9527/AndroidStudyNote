c89e55b0be639f52c7ebb05af4e828cd81dd90d1
lintex9527 <lintex9527@yeah.net>
2018-08-17 13:30:37 +0800

    添加XLog支持，调试应用更加方便

--- gamecenter/src/main/java/com/lintex9527/android/gamecenter/MainActivity.java
+++ gamecenter/src/main/java/com/lintex9527/android/gamecenter/MainActivity.java
@@ -1,7 +1,10 @@
 package com.lintex9527.android.gamecenter;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+import com.elvishew.xlog.Logger;
+import com.elvishew.xlog.XLog;
 
 /**
  * 模拟一个登录界面，有以下几点要求：
@@ -15,10 +18,20 @@ import android.os.Bundle;
  * 7、登录功能：验证用户名和对应的密码是否存在，二者都匹配则跳转到“欢迎老用户界面”；失败则提示密码和用户名不匹配（需要检查是否为空）；
  */
 public class MainActivity extends AppCompatActivity {
-
+    
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        XLog.d("onCreate() 主界面初始化");
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        // 另外一种局部使用XLog的方式
+        Logger logger = XLog.tag("destroy").build();
+        logger.d("onDestory() 销毁活动");
+
     }
 }
